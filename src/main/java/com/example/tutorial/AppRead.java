38d7e404c1cf2bfcf2347c6955f483bb8bdc2444
niejinping <niejinping@sanhao.com>
2016-05-12 09:45:23 +0800

    first commit

---
+++ src/main/java/com/example/tutorial/AppRead.java
@@ -0,0 +1,30 @@
+package com.example.tutorial;
+
+import com.example.tutorial.AddressBookProtos.AddressBook;
+import com.example.tutorial.AddressBookProtos.Person;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * Created by njp on 2016/5/11.
+ */
+public class AppRead {
+    public static void main(String[] args ) throws Exception{
+        FileInputStream fi = new FileInputStream("hello");
+        AddressBook ab = AddressBook.parseFrom(fi);
+
+        Person person = ab.getPerson(0);
+
+        System.out.println(person.getId());
+        System.out.println(person.getName());
+        System.out.println(person.getEmail());
+
+        Person.PhoneNumber phoneNumber = person.getPhone(0);
+
+        System.out.println(phoneNumber.getNumber());
+
+
+        fi.close();
+    }
+}
